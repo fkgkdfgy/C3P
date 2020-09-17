e617c053f4519954a1d63b9ae979b5ca2cdb6942
Liu Weilong <weilong.liu-csu@outlook.com>
2020-09-17 17:25:19 +0800

    add lamda decltype test

---
+++ CPP11/decltype_and_lamda/lamdda-test.cpp
@@ -0,0 +1,20 @@
+/*
+ * @Author: Liu Weilong
+ * @Date: 2020-09-17 15:33:42
+ * @LastEditors: Liu Weilong 
+ * @LastEditTime: 2020-09-17 15:36:12
+ * @FilePath: /C3P/CPP11/decltype_and_lamda/lamdda-test.cpp
+ * @Description: 主要是用于测试lamda的一些性质
+ */
+
+// 用途: 1.查看lamda的捕捉(主要用途)
+//      2. 查看default ctor 和空ctor的初始化区别
+class LamdaTestCase
+{
+    public:
+
+    private:
+
+    int a;
+
+};
